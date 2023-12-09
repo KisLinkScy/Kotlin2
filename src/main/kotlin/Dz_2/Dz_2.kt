c9e189f96f39ac40aa1e96eb4cc1a6ca8c1f6035
KisLinkSky <serega02211@gmail.com>
2023-12-09 22:32:27 +0400

    dz_3

--- src/main/kotlin/Dz_2/Dz_2.kt
+++ src/main/kotlin/Dz_2/Dz_2.kt
@@ -1,2 +1,13 @@
 package Dz_2
 
+fun main() {
+    println("Задание №2")
+    val hobbits = arrayOf("Frodo", "Sam", "Merry", "Pippin")
+    var x = 0
+
+    while (x < 4) {
+        println("| ${hobbits[x]} is a good Hobbit name: |")
+        println("--------------------------------")
+        x += 1
+    }
+}
